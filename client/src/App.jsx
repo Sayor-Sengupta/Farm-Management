a3ffec87598e487d3c27ba697605e9f0c35f6b15
Puskar_Das <puskardas28@gmail.com>
2024-09-06 17:02:41 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
    <>
    <h1>Farm Management</h1>
+   <h1>Farm Management</h1>
+   <h1>Farm Management</h1>
+
+
    </>
   )
 }
