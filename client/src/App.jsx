3f9a2a1c7f151451fa8040bd15f11aff7b6b982f
sayor-sengupta <sayorsengupta9@gmail.com>
2024-09-21 20:24:47 +0530

    sidebar added along with file format change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './components/Side Bar/Sidebar'
+import HomePage from './pages/HomePage'
 
 function App() {
   
 
   return (
    <>
-   <h1>Farm Management</h1>
-   <h1>Farm Management</h1>
-   <h1>Farm Management</h1>
+   <HomePage/>
+
 
 
    </>
