23ae2f713f403782cbedf4b1fb59e6a2de9b5624
sayor-sengupta <sayorsengupta9@gmail.com>
2024-10-17 22:56:36 +0530

    changes in server.js and other package.json updates

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-
-
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; 
+import cors from "cors";
+import  connectMongoDb  from "./utils/connectMongoDb.js";
+import authRoutes from './routes/auth.routes.js'
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000   ; 
+
+ 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser()); 
 app.use(express.json());
+app.use('/api/users',authRoutes)
 
-app.use("/auth", authRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  connectMongoDb()
+  console.log(`server is running ${PORT}`);
 });
