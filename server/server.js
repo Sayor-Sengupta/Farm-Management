5a5225e40df269be5420f7411ce410f213833bda
sayor-sengupta <sayorsengupta9@gmail.com>
2024-10-23 01:56:50 +0530

    updates in package.json and server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import  connectMongoDb  from "./utils/connectMongoDb.js";
 import authRoutes from './routes/auth.routes.js'
+import ecommerceRoutes from './routes/product.routes.js'
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000   ; 
@@ -13,6 +14,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser()); 
 app.use(express.json());
 app.use('/api/users',authRoutes)
+app.use('/api/Ecom',ecommerceRoutes)
 
 
 app.listen(PORT, () => {
